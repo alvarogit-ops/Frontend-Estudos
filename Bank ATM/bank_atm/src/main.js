bbab063554afaaa4fb932740a2f788b7305ef868
Alvaro Yuri <alvaro.yuri@outlook.com>
2026-09-05 12:43:24 -0300

    Iniciando meu projeto de saque bancário

---
+++ Bank ATM/bank_atm/src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import heroImg from './assets/hero.png'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+<section id="center">
+  <div>
+    <h1>Caixa Eletrônico</h1>
+</section>
+
+`
